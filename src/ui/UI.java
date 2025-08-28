e7970314949a632d6abf9261ec55c79fa3be64cb
Agustín <agustin.guzman.v@proton.me>
2025-08-28 19:07:09 -0400

    people ui conected to the program

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -1,21 +1,25 @@
 package ui;
 
 import ui.modules.CalendarUI;
+import ui.modules.PeopleUI;
 import modules.calendar.Calendar;
+import modules.people.People;
 
 import java.util.Scanner;
 
 public class UI {
-    public static void run(Calendar calendar) {
+    public static void run(Calendar calendar, People people) {
         Scanner scan = new Scanner(System.in);
         CalendarUI calendarUI = new CalendarUI();
+        PeopleUI peopleUI = new PeopleUI();
         boolean running = true;
 
         while (running) {
             System.out.println("=====> Main Menu <=====");
             System.out.println("What do you want to do?");
             System.out.println("1. Calendar.");
-            System.out.println("2. Exit.");
+            System.out.println("2. People.");
+            System.out.println("3. Exit.");
             System.out.print("Select an option: ");
 
             int option;
@@ -32,7 +36,10 @@ public class UI {
                 case 1: // calendario
                     calendarUI.run(scan, calendar);
                     break;
-                case 2: // salir de la interfaz
+                case 2: // personas
+                    peopleUI.run(scan, people);
+                    break;
+                case 3: // salir de la interfaz
                     System.out.println("Goodbye.");
                     running = false;
                     break;
