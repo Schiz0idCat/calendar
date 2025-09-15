07702d2282718dc7fc751126b0b55c4a6621d15e
Agustín <agustin.guzman.v@proton.me>
2025-09-15 12:58:47 -0300

    file manager abstract class

---
+++ src/disk/FileManager.java
@@ -0,0 +1,55 @@
+package disk;
+
+import net.harawata.appdirs.AppDirs;
+import net.harawata.appdirs.AppDirsFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public abstract class FileManager<T> {
+    public enum FileType {
+        CONFIG, DATA
+    }
+
+    protected final Path filePath;
+
+    public FileManager(String appName, String fileName, FileType fileType) throws IOException {
+        this.filePath = getDefaultPath(appName, fileName, fileType);
+        createFileIfNotExists();
+    }
+
+    private static Path getDefaultPath(String appName, String fileName, FileType fileType) throws IOException {
+        AppDirs appDirs = AppDirsFactory.getInstance();
+        String baseDir;
+
+        switch (fileType) {
+            case CONFIG:
+                baseDir = appDirs.getUserConfigDir(appName, null, null);
+                break;
+            case DATA:
+                baseDir = appDirs.getUserDataDir(appName, null, null);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown file type: " + fileType);
+        }
+
+        Path path = Path.of(baseDir, fileName);
+
+        if (!Files.exists(path.getParent())) {
+            Files.createDirectories(path.getParent());
+        }
+
+        return path;
+    }
+
+    private void createFileIfNotExists() throws IOException {
+        if (!Files.exists(filePath)) {
+            Files.createFile(filePath);
+        }
+    }
+
+    public abstract void save(T data);
+
+    public abstract T load();
+}
